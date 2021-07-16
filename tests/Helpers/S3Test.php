98c53e7e3fe747a5ec571f04c52e784db82c5012
quentin.schmick <quentin.schmick@nathanjames.com>
2021-07-16 08:59:53 -0400

    Fixing test

--- tests/Helpers/S3Test.php
+++ tests/Helpers/S3Test.php
@@ -22,6 +22,6 @@ class S3Test extends TestCase
         $this->assertNull(S3::getBucketFromPath('s3.com/path/to/file.csv'));
         $this->assertNull(S3::getBucketFromPath('path/to/file.csv'));
         $this->assertNull(S3::getBucketFromPath('/path/to/file.csv'));
-        $this->assertEquals('sb-us-east-staging', S3::getBucketFromPath('https://test-us-east-staging.s3.us-east-2.amazonaws.com/test_import.xlsx'));
+        $this->assertEquals('test-us-east-staging', S3::getBucketFromPath('https://test-us-east-staging.s3.us-east-2.amazonaws.com/test_import.xlsx'));
     }
 }
