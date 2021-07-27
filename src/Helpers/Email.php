0ed37173badd8b8c1201fabc3cd4db391fbac2b7
quentin.schmick <quentin.schmick@nathanjames.com>
2021-07-27 09:19:39 -0400

    Added simple ability to normalize email addresses

--- src/Helpers/Email.php
+++ src/Helpers/Email.php
@@ -4,7 +4,40 @@ namespace BluefynInternational\Sidekick\Helpers;
 
 class Email
 {
-    public static function normalize(String $emailAddress) : string
+    const DOMAIN_ALIASES = [
+        'googlemail.com' => [
+            'alias_for' => 'gmail.com',
+        ],
+        'me.com' => [
+            'alias_for' => 'icloud.com',
+        ],
+        'mac.com' => [
+            'alias_for' => 'icloud.com',
+        ],
+        'yahoo.com.cn' => [
+            'alias_for' => 'yahoo.cn',
+        ],
+        'yahoo.co.in' => [
+            'alias_for' => 'yahoo.co.in',
+        ],
+        'yandex.ru' => [
+            'alias_for' => 'yandex.com',
+        ],
+        'yandex.by' => [
+            'alias_for' => 'yandex.com',
+        ],
+        'yandex.ua' => [
+            'alias_for' => 'yandex.com',
+        ],
+        'yandex.kz' => [
+            'alias_for' => 'yandex.com',
+        ],
+        'ya.ru' => [
+            'alias_for' => 'yandex.com',
+        ],
+    ];
+
+    public static function normalizeFullEmail(String $emailAddress) : string
     {
         if (!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
             throw new \InvalidArgumentException('Invalid email address');
@@ -23,6 +56,12 @@ class Email
     {
         $domainParts = explode('.', mb_strtolower($emailDomain));
 
-        return implode('.', array_splice($domainParts, -2, 2));
+        $normalizedDomain = implode('.', array_splice($domainParts, -2, 2));
+
+        if (isset(self::DOMAIN_ALIASES[$normalizedDomain])) {
+            return self::DOMAIN_ALIASES[$normalizedDomain]['alias_for'];
+        }
+
+        return $normalizedDomain;
     }
 }
--- tests/Helpers/EmailTest.php
+++ tests/Helpers/EmailTest.php
@@ -10,37 +10,48 @@ class EmailTest extends TestCase
     /** @test */
     public function withDots()
     {
-        $this->assertEquals('testemail@domain.com', Email::normalize('te.st.email@domain.com'));
+        $this->assertEquals('testemail@domain.com', Email::normalizeFullEmail('te.st.email@domain.com'));
     }
 
     /** @test */
     public function withDashes()
     {
-        $this->assertEquals('testemail@domain.com', Email::normalize('test-email@domain.com'));
+        $this->assertEquals('testemail@domain.com', Email::normalizeFullEmail('test-email@domain.com'));
     }
 
     /** @test */
     public function withSubDomain()
     {
-        $this->assertEquals('testemail@domain.com', Email::normalize('test.email@sub.domain.com'));
+        $this->assertEquals('testemail@domain.com', Email::normalizeFullEmail('test.email@sub.domain.com'));
     }
 
     /** @test */
     public function withCapitals()
     {
-        $this->assertEquals('testemail@domain.com', Email::normalize('TeStEmail@DomAin.cOm'));
+        $this->assertEquals('testemail@domain.com', Email::normalizeFullEmail('TeStEmail@DomAin.cOm'));
     }
 
     /** @test */
     public function withEverything()
     {
-        $this->assertEquals('testemail@domain.com', Email::normalize('TeS.-tE-mail@multi.sub.sub.DomAin.cOm'));
+        $this->assertEquals('testemail@domain.com', Email::normalizeFullEmail('TeS.-tE-mail@multi.sub.sub.DomAin.cOm'));
     }
 
     /** @test */
     public function invalidAddress()
     {
         $this->expectException(\InvalidArgumentException::class);
-        Email::normalize('TeS.-tE-mail@not.1');
+        Email::normalizeFullEmail('TeS.-tE-mail@not.1');
+    }
+
+    /** @test */
+    public function aliasedDomain()
+    {
+        $normalizedEmailOne = Email::normalizeFullEmail('TeS.tEmail@me.COM');
+        $normalizedEmailTwo = Email::normalizeFullEmail('Te-StEm.ail@icloud.COM');
+        $normalizedEmailThree = Email::normalizeFullEmail('TeS.tEma-il@mAc.com');
+
+        $this->assertEquals($normalizedEmailOne, $normalizedEmailTwo);
+        $this->assertEquals($normalizedEmailTwo, $normalizedEmailThree);
     }
 }
