3cb903c57ec57e39f26b7c151e61b39f08900380
quentin.schmick <quentin.schmick@nathanjames.com>
2021-10-14 09:39:29 -0400

    Make the ordered observer move records out of the way when getting inserted/updated

--- src/Observers/OrderedObserver.php
+++ src/Observers/OrderedObserver.php
@@ -2,6 +2,7 @@
 
 namespace BluefynInternational\Sidekick\Observers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderedObserver
@@ -11,5 +12,36 @@ class OrderedObserver
         if (null === $saving->{$saving::$_orderedColumn}) {
             $saving->{$saving::$_orderedColumn} = $saving::max($saving::$_orderedColumn) + 1;
         }
+    }
+
+    public function creating(Model $creating)
+    {
+        if ($creating->isDirty($creating::$_orderedColumn)) {
+            $this->updateImpactedOrderedSiblings($creating);
+        }
+    }
+
+    public function updating(Model $updating)
+    {
+        if ($updating->isDirty($updating::$_orderedColumn)) {
+            $this->updateImpactedOrderedSiblings($updating);
+        }
+    }
+
+    protected function updateImpactedOrderedSiblings(Model $model)
+    {
+        $oldSort = $model->getOriginal($model::$_orderedColumn) ?? PHP_INT_MAX;
+        $newSort = $model->{$model::$_orderedColumn};
+        $adjustmentAmount = $newSort > $oldSort ? -1 : 1;
+
+        app(get_class($model))->where($model::$_orderedColumn, '>=', min($oldSort, $newSort))
+            ->where($model::$_orderedColumn, '<=', max($oldSort, $newSort))
+            ->when($model->getKey(), function (Builder $builder) use ($model) {
+                $builder->where($model->getKeyName(), '!=', $model->getKey());
+            })
+            ->each(function (Model $instance) use ($adjustmentAmount) {
+                $instance->{$instance::$_orderedColumn} += $adjustmentAmount;
+                $instance->saveQuietly();
+            });
     }
 }
